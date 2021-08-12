578cd5fa848e055bb928e924d8386c2f68550cdd
Ayush Dubey <ayushdubey70@gmail.com>
2021-08-12 22:57:53 +0530

    lib: post tweets using twitter api
    *catch error responses
    *show no internet connection on socket failure
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/twtr_repository.dart
+++ lib/twtr_repository.dart
@@ -20,3 +20,25 @@ class Failure {
   Failure(this.message);
   final String message;
 }
+
+final twitterRepositoryProvider = Provider<TwitterRepository>((ref) {
+  final twtrApi = ref.watch(twtrApiProvider);
+
+  return TwitterRepository(twtrApi);
+});
+
+class TwitterRepository {
+  TwitterRepository(this._twtrApi);
+  final TwitterApi _twtrApi;
+
+  Future<Either<Failure, String>> post(String status) async {
+    try {
+      final tweet = await _twtrApi.tweetService.update(status: status);
+      return Right(tweet.fullText);
+    } on Response catch (response) {
+      return Left(Failure(response.reasonPhrase));
+    } on SocketException catch (_) {
+      return Left(Failure('No Internet Connection!'));
+    }
+  }
+}
