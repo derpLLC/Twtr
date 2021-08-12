1923fc754d94a4ee3cd1677f1d0d2a524abb2613
Ayush Dubey <ayushdubey70@gmail.com>
2021-08-12 22:10:56 +0530

    lib: use provider to pass configs
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/environment_config.dart
+++ lib/environment_config.dart
@@ -1,8 +1,12 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class EnvironmentConfig {
-  final apiKey =  const String.fromEnvironment("apiKey");
+  final apiKey = const String.fromEnvironment("apiKey");
   final apiKeySecret = const String.fromEnvironment("apiKeySecret");
   final accessToken = const String.fromEnvironment("accessToken");
   final accessTokenSecret = const String.fromEnvironment("accessTokenSecret");
 }
+
+final environmentConfigProvider = Provider<EnvironmentConfig>((ref) {
+  return EnvironmentConfig();
+});
