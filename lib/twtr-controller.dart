8a3961b176f0d360d4d85f5949fc99b82f2af84f
Ayush Dubey <ayushdubey70@gmail.com>
2021-08-13 11:57:08 +0530

    lib: post status updates
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ lib/twtr-controller.dart
@@ -0,0 +1,24 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:twtr/twtr_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class TwitterController extends StateNotifier<AsyncValue<String>> {
+  TwitterController(
+    this._twitterRepository, [
+    AsyncValue<String>? state,
+  ]) : super(state ?? AsyncValue.data(''));
+  final TwitterRepository _twitterRepository;
+
+  Future<Either<Failure, String>> postTweet(String status) async {
+    state = AsyncValue.loading();
+
+    final result = await _twitterRepository.post(status);
+
+    result.fold(
+      (failure) => state = AsyncValue.error(failure),
+      (message) => state = AsyncValue.data(message),
+    );
+
+    return result;
+  }
+}
